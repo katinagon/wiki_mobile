7f0b09bc26d6be2184b1df67c0d3ef568372a553
Ekaterina Goncharova <katinagon@yandex.ru>
2025-07-29 11:34:10 +0300

    Added interface

--- src/test/java/drivers/BrowserStackDriver.java
+++ src/test/java/drivers/BrowserStackDriver.java
@@ -1,10 +1,15 @@
 package drivers;
 
 import com.codeborne.selenide.WebDriverProvider;
+import helpers.MobileConfig;
+import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import javax.annotation.Nonnull;
+import java.net.URL;
 
 public class BrowserStackDriver implements WebDriverProvider {
     @Nonnull
--- src/test/java/helpers/MobileConfig.java
+++ src/test/java/helpers/MobileConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.Sources({
         "file:src/test/resources/${platform}.properties"
 })
-public class MobileConfig extends Config {
+public interface MobileConfig extends Config {
     @Key("platformName") String platformName();
     @Key("device") String device();
     @Key("os_version") String osVersion();
